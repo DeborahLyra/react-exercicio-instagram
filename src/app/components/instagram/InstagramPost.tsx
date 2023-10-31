da8d4302d18cce877ea9264245dc9bf42a0518dd
Deborah Lyra <deborahp.lyra@gmail.com>
2023-10-30 21:03:45 -0300

    finalizando o inicio -  usando props

--- src/app/components/instagram/InstagramPost.tsx
+++ src/app/components/instagram/InstagramPost.tsx
@@ -1,7 +1,35 @@
-function InstagramPost (){
+interface propsData {
+   profilePicture:  string,
+    name:  string, 
+   location: string,  
+    postPicture: string,  
+}
+
+function InstagramPost ({profilePicture, name , location, postPicture}: propsData){
  return(
-    <>
-    </>
+   <main>
+   <section className="container">
+     <div className="data-container">
+       <img
+         src={profilePicture}
+         alt="perfil"
+         className="profilePicture"
+       />
+       <div className="profile-text-container">
+         <h4 className="profileName">{name}</h4>
+         <p className="location">{location}</p>
+       </div>
+     </div>
+     <img
+       src= {postPicture}
+       alt=""
+       className="post-image"
+     />
+     <div className="likes-container">
+       curtidas e comentários 
+     </div>
+   </section>
+ </main>
  )
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,26 +2,13 @@ import InstagramPost from "./components/instagram/InstagramPost";
 
 export default function Home() {
   return (
-    <main>
-      <section>
-        <div className="data-container">
-          <img
-            src="https://cdn-icons-png.flaticon.com/512/3135/3135768.png"
-            alt="perfil"
-            className="profilePicture"
-          />
-          <div>
-            <h3 className="profileName">Nome</h3>
-            <p className="location">Localização</p>
-          </div>
-        </div>
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeNi-Oz3UZSl4cQli_D0FmVTZhUUaGxviO2g&usqp=CAU"
-          alt=""
-          className="post-image"
-          
-        />
-      </section>
-    </main>
+  <>
+  <InstagramPost 
+    profilePicture = {"https://cdn-icons-png.flaticon.com/512/3135/3135768.png"}
+    name= "Mary"
+    location= "Brasil"
+    postPicture= {"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeNi-Oz3UZSl4cQli_D0FmVTZhUUaGxviO2g&usqp=CAU"}
+  />
+  </>
   );
 }
